7f2c5dd04f18d274098ff29e9768099ce21fd881
killer-123-cpu <etimg112@gmail.com>
2025-07-21 17:31:59 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Jarvis",
   botOwner: 'godyrock', // Your name
-  ownerNumber: '9152300835', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+2349152300835', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
